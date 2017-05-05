4b61c3bb2fd2b0d98a363711560921facad097f6
Joel Gruselius <joel.gruselius@scilifelab.se>
2017-05-05 10:31:31 +0200

    Fix CSV formatting

--- supernova_report_parser.py
+++ supernova_report_parser.py
@@ -1,48 +1,42 @@
 import sys
 import re
 import json
+import csv
 import fileinput
 import argparse
 
-def print_json(files):
+def parse_and_print(files, print_csv=False):
 	samples = {}
+	p = re.compile(r"\[(P\d{3,}_\d{3,})\]")
 	for line in fileinput.input(files=files):
-		m = re.search(r"P\d{3,}_\d{3,}", line)
+		m = p.search(line)
 		if m:
-			s = m.group()
+			s = m.group(1)
 		else:
 			l = line.split("=")
 			if len(l) > 2:
 				d = [x.strip(" -") for x in l]
 				samples.setdefault(s, {})[d[1]] = d[0]
-
-	print(json.dumps(samples))
-
-def print_csv(files):
-	samples = {}
-	for line in fileinput.input(files=files):
-		m = re.search(r"P\d{3,}_\d{3,}", line)
-		if m:
-			samples.setdefault("SAMPLE", []).append(m.group())
-		else:
-			l = line.split("=")
-			if len(l) > 2:
-				d = [x.strip(" -") for x in l]
-				samples.setdefault(d[1], []).append(d[0])
-
-	data = []
-	for k,v in samples.items():
-		data.append([k] + v)
-
-	data = list(map(list, zip(*data))) # Transpose
-	for row in data:
-		print(",".join(row))
+	if print_csv:
+		# Convert to list:
+		slist = []
+		for k,v in samples.items():
+			x = {"SAMPLE": k}
+			x.update(v)
+			slist.append(x)
+		# Build header from all unique labels:
+		header = set()
+		for s in slist:
+			header.update(s.keys())
+		writer = csv.DictWriter(sys.stdout,fieldnames=list(header))
+		writer.writeheader()
+		for s in slist:
+			writer.writerow(s)
+	else:
+		print(json.dumps(samples))
 
 def main(args):
-	if args.csv:
-		print_csv(args.files)
-	else:
-		print_json(args.files)
+	parse_and_print(args.files, args.csv)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Parse Supernova reports and print numbers in JSON or CSV")
